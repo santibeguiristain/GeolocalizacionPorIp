712aa1ad823eec706c82a4cb7acf88f8aa81651e
sbeguiristain <sbeguiristain@domsf.com>
2023-09-24 10:52:47 -0300

    devolviendo latitude y longitude

--- client/index.js
+++ client/index.js
@@ -1,3 +1,6 @@
+ 
+
+
 function soloNumeros(event) {
     // Permitir solo números y la tecla de retroceso
     const key = event.key;
@@ -14,3 +17,35 @@ function mostrarNumero() {
     const numero = nroIp.value;
     resultadoSpan.textContent = numero;
 }
+
+
+
+  // Agrega JavaScript para manejar el evento de clic en el botón
+  document.getElementById('obtenerDatos').addEventListener('click', function () {
+    // Realiza una solicitud al servidor cuando se hace clic en el botón
+    axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=83572ddf5cb44cb5860e0c8ebbf8a6f5')
+        .then(response => {
+            const data = response.data;
+
+            // Obtén elementos HTML por su ID
+            const nombreElement = document.getElementById('nombre');
+            const ipAddressElement = document.getElementById('ipAddress');
+            const cityElement = document.getElementById('city');
+            const longitudeElement = document.getElementById('longitude');
+            const latitudeElement = document.getElementById('latitude');
+
+            // Asigna los valores a los elementos HTML
+            nombreElement.textContent = 'Nombre: ' + data.country;
+            ipAddressElement.textContent = 'IP Address: ' + data.ip_address;
+            cityElement.textContent = 'City: ' + data.city;
+            longitudeElement.textContent = 'Longitude: ' + data.longitude;
+            latitudeElement.textContent = 'Latitude: ' + data.latitude;
+
+           
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
